9e78fed3ac81419fbcff73bf22b9354254d3fe03
Lakhan Singh <lakhans671@gmail.com>
2020-08-11 22:54:44 +0530

    python basic

---
+++ python/IfElseDemo.py
@@ -0,0 +1,6 @@
+salary=30000
+if(salary>200000):
+    print("yes this greater")
+else:
+  print("Rejected")    
+ 
---
+++ python/hello.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+print("Hello world")
+a="hello World"
+print(a)
+for i in a:
+    print(i);
+    
+var1=10;
+var2=10.1
+print(var1,var2)
+var3=True
+var4=False
